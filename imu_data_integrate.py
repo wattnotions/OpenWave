4a7ad39380a13b44c33a44cbaabfed7355176f8c
shane <shane@localhost.localdomain>
2019-02-28 12:11:45 +0000

    chunk_integrate function first revision completed and working

--- imu_data_integrate.py
+++ imu_data_integrate.py
@@ -8,7 +8,6 @@ import sys
 fs = 19.23 # Sampling frequency
 
 
-dx_times = []
 
 ### open csv file and get required parameters ###
 def get_csv_data():
@@ -29,14 +28,18 @@ def get_csv_data():
 ### convert the millisecond timestamps into a suitable time x axis ###
 
 def format_millis_to_xaxis(timestamps, scale_factor):
+	dx_times = []
+
 	array_length = len(timestamps)
 	dx_times.append(0.00)
 	for idx in range(array_length-1):
-		dx_times.append( (float(timestamps[idx+1]) - float(timestamps[0]))/scale_factor)##make scale factor 1000 to go from secods to milliseconds
+		dx_time = ( (float(timestamps[idx+1]) - float(timestamps[0]))/scale_factor)##make scale factor 1000 to go from secods to milliseconds
+		dx_time = round(dx_time, 8)
+		dx_times.append(dx_time)
 	
 	return dx_times
 	
-x_axis = copy.copy(dx_times)
+#x_axis = copy.copy(dx_times)
 
 ### filter the acceleration data ###
 def filter_accel_data(z_accels):
@@ -67,12 +70,42 @@ def chunks(l, n):
 
 
 ### integrate the acceleration data in chunks ###
-def chunk_integrate():        
-	velocity_chunk = []
-	location_chunk = []
-	for h in range(len(dx_times)):
-		velocity_chunk = it.cumtrapz(filtered_z_axis,dx_times)
-		location_chunk = it.cumtrapz(velocity,dx_times)
+def chunk_integrate(dx_times, filtered_z_axis):        
+	z_accels_chunks = []
+	x_axis_chunks   = []
+	zeroed_x_axis   = []
+	velocity_chunk  = []
+	location_chunk  = []
+
+	for x in chunks(filtered_z_axis, 77):
+		z_accels_chunks.append(x)
+		
+	for y in chunks(dx_times, 77):
+		x_axis_chunks.append(y)
+		
+		
+	for i in x_axis_chunks[:10]:
+		zeroed_x_axis.append((format_millis_to_xaxis(i,1)))
+		
+	print len(z_accels_chunks)
+	print len(x_axis_chunks)
+
+	for idx, h in enumerate(x_axis_chunks):
+		velocity_chunk.append(it.cumtrapz(z_accels_chunks[idx],h))
+		location_chunk.append(it.cumtrapz(velocity_chunk[-1],h[:-1]))
+
+	stitched_location = []	
+	for h in location_chunk:
+		for x in h:
+			stitched_location.append(x)
+		
+		
+	plt.plot(dx_times[:1576], stitched_location, label='location')
+	plt.ylabel('Displacement (m)')
+	plt.xlabel('Time (Seconds)')
+	plt.legend()
+	plt.show()
+		
 
 
 ### plot the data ###
@@ -97,39 +130,6 @@ def plot_data(z_accels, filtered_z_axis, velocity, location, dx_times):
 timestamps, z_accels = get_csv_data()
 dx_times = format_millis_to_xaxis(timestamps, 1000)
 filtered_z_axis = filter_accel_data(z_accels)
-
-###prototype chunk integration code###
-z_accels_chunks = []
-x_axis_chunks   = []
-zeroed_x_axis   = []
-velocity_chunk  = []
-location_chunk  = []
-
-for x in chunks(filtered_z_axis, 77):
-	z_accels_chunks.append(x)
-	
-for y in chunks(dx_times, 77):
-	x_axis_chunks.append(y)
-	
-print len(filtered_z_axis)
-print len(dx_times)
-	
-for i in x_axis_chunks[:2]:
-	print i
-	zeroed_x_axis.append(format_millis_to_xaxis(i,1))
-	
-print zeroed_x_axis[:2]
-
-sys.exit()
-		
-for h in zeroed_x_axis:
-	print len(h)
-	print len(z_accels_chunks)
-	velocity_chunk.append(it.cumtrapz(z_accels_chunks,h))
-	location_chunk.append(it.cumtrapz(velocity,h[:-1]))
-
-###
-	
-
-#velocity, location = double_integrate_data(filtered_z_axis, dx_times)
-#plot_data(z_accels, filtered_z_axis, velocity, location, dx_times)
+chunk_integrate(dx_times, filtered_z_axis)
+velocity, location = double_integrate_data(filtered_z_axis, dx_times)
+plot_data(z_accels, filtered_z_axis, velocity, location, dx_times)
