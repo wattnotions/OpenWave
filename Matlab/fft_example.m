10ce093423d6b83bab5bbee4c44c3543843bc16d
wattnotions <shaneormonde@gmail.com>
2019-11-26 10:30:29 +0000

    add fft_example for matlab website

---
+++ Matlab/fft_example.m
@@ -0,0 +1,19 @@
+Fs = 1000;            % Sampling frequency                    
+T = 1/Fs;             % Sampling period       
+L = 1500;             % Length of signal
+t = (0:L-1)*T;        % Time vector
+
+S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t); %make sine signal with 50 and 120Hz components
+
+Y=fft(S);
+
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
