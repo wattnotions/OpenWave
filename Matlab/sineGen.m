660d8ffd6fdba065e6a133d463350760cffa4f8c
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-17 13:01:39 +0000

    added stoptime argument

--- Matlab/sineGen.m
+++ Matlab/sineGen.m
@@ -1,9 +1,8 @@
-function y = sineGen(amplitude, freq, phase)
+function y = sineGen(amplitude, freq, phase, stoptime)
 
     fs = 2048; % Sampling frequency (samples per second) 
     dt = 1/fs; % seconds per sample 
-    StopTime = 0.25; % seconds 
-    t = (0:dt:StopTime)'; % seconds 
+    t = (0:dt:stoptime)'; % seconds 
     F = freq; % Sine wave frequency (hertz)
     p_shift = deg2rad(phase);
     y = sin(amplitude*pi*F*t + p_shift);
