7b55dd4ef832fbef326559ef561cef15b012033d
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-18 14:50:59 +0000

    replaced hardcoded values with variables

--- Matlab/sig_wave_gen.m
+++ Matlab/sig_wave_gen.m
@@ -1,27 +1,36 @@
+num_waves = 100; %Number of sine waves to create and add together
+
 %Generate wave amplitudes based on normal distribution
-r = normrnd(0,10,[1,100]); %Generate random numbers
+r = normrnd(0,10,[1,num_waves]); %Generate random numbers
 amps = r + abs(min(r));        %shift values to positive numbers
 %histogram(amps, 100);
 std(amps)
 
 
-%Generate random frequencies
+%Generate random frequencies between 0.05-0.67Hz
 min_freq=0.05;
 max_freq=0.67;
-n=100;
+n=num_waves;
 freqs=min_freq+rand(1,n)*(max_freq-min_freq);
 
 %Generate random phase shifts
 min_phase=0;
 max_phase=180;
-n=100;
+n=num_waves;
 phases=min_phase+rand(1,n)*(max_phase-min_phase);
 
-waves = zeros(100, 204801);
+
+
+fs = 160;         %sampling frequency in hz
+stoptime = 100; %sample length in seconds
+
+waves = zeros(100, (fs*stoptime)+1);  %define matrix to store sine wave vectors in
 
 for i = 1:100 
-   waves(i,:) = sineGen(amps(i), freqs(i), phases(i), 100);
+   waves(i,:) = sineGen(amps(i), freqs(i), phases(i), 100, fs);
 end
+
+
 
 
 
