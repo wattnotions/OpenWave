0c729d5e3d53f02a7a87b1870c2f28d30d74f5a4
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-18 16:55:40 +0000

    add all waves together and plot done

--- Matlab/sig_wave_gen.m
+++ Matlab/sig_wave_gen.m
@@ -1,9 +1,10 @@
-num_waves = 1; %Number of sine waves to create and add together
+num_waves = 1000; %Number of sine waves to create and add together
 
 %Generate wave amplitudes based on normal distribution
-r = normrnd(0,10,[1,num_waves]); %Generate random numbers
+r = normrnd(0,5,[1,num_waves]); %Generate random numbers
 amps = r + abs(min(r));        %shift values to positive numbers
-%histogram(amps, 100);
+
+histogram(amps, 100);
 std(amps)
 
 
@@ -15,22 +16,29 @@ freqs=min_freq+rand(1,n)*(max_freq-min_freq);
 
 %Generate random phase shifts
 min_phase=0;
-max_phase=180;
+max_phase=360;
 n=num_waves;
 phases=min_phase+rand(1,n)*(max_phase-min_phase);
 
 
 
-fs = 2;         %sampling frequency in hz
-stoptime = 1000; %sample length in seconds
+fs = 200;         %sampling frequency in hz
+stoptime = 50; %sample length in seconds
+num_samples =  (fs*stoptime)+1;
 
-waves = zeros(num_waves, (fs*stoptime)+1);  %define matrix to store sine wave vectors in
+waves = zeros(num_waves, num_samples);  %define matrix to store sine wave vectors in
 
 for i = 1:num_waves 
    waves(i,:) = sineGen(amps(i), freqs(i), phases(i), stoptime, fs);
 end
 
+sim_wave = zeros(1,num_samples);
 
+for i = 1:num_waves
+    sim_wave = sim_wave + waves(i,:);
+end
 
-
+std(amps)
+std(sim_wave)
+%plot(sim_wave)
 
