706a610ee9396d6e8b14e70c741b0c78dbf2c8c4
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-20 16:13:35 +0000

    added ylines to plot

--- Matlab/gen_and_plot.m
+++ Matlab/gen_and_plot.m
@@ -6,31 +6,21 @@ sample_length = (fs*stoptime)+1;
 
 
 S = oceanWaveSim(num_waves, 1, fs, stoptime);
-plotFFT(fs, sample_length, S);
+%plotFFT(fs, sample_length, S);
 
 
 %Plot surface displacement
-figure(3)
+%figure(3)
 t = (0:dt:stoptime)'; % seconds 
+plot(t,S);
 
-%plot(t,S);
+sig_wave_height = sigWaveCalc(S);
+fprintf('Simulated wave sig wave height (highest 3rd) = %f\n',sig_wave_height);
+
+yline(sig_wave_height/2,'--g','Hightest 3rd Calc');
+yline((4*std(S))/2,'b','4*std Calc');
+
+
 %yline((4*std(S))- abs(min(S)));
 % Find local maxima and minima
-maxIndices = islocalmax(S);
-minIndices = islocalmin(S);
 
-% Visualize results
-clf
-plot(S,'Color',[109 185 226]/255,'DisplayName','Input data')
-hold on
-
-% Plot local maxima
-plot(find(maxIndices),S(maxIndices),'^','Color',[217 83 25]/255,...
-    'MarkerFaceColor',[217 83 25]/255,'DisplayName','Local maxima')
-
-% Plot local minima
-plot(find(minIndices),S(minIndices),'v','Color',[237 177 32]/255,...
-    'MarkerFaceColor',[237 177 32]/255,'DisplayName','Local minima')
-title(['Number of extrema: ' num2str(nnz(maxIndices)+nnz(minIndices))])
-hold off
-legend
