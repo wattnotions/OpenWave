768d9467ad813176118a0a8e7590870caf3cca63
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-20 16:37:04 +0000

    added % difference calc

--- Matlab/gen_and_plot.m
+++ Matlab/gen_and_plot.m
@@ -10,8 +10,12 @@ S = oceanWaveSim(num_waves, 1, fs, stoptime);
 
 
 sig_wave_height = sigWaveCalc(S);
+sig_wave_std_dev = 4*std(S);
 fprintf('Simulated wave sig wave height (4*std_dev) = %f\n',4*std(S));
 fprintf('Simulated wave sig wave height (highest 3rd) = %f\n',sig_wave_height);
+
+percent_diff = ((sig_wave_std_dev - sig_wave_height) / sig_wave_height)*100;
+fprintf('Percent difference = %f\n', percent_diff);
 
 yline(sig_wave_height/2,'--g','Hightest 3rd Calc');
 yline((4*std(S))/2,'b','4*std Calc');
