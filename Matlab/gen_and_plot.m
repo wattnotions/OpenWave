71629276e92b06a408aa14888f158363d2fe5a71
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-20 16:21:40 +0000

    removed debug code

--- Matlab/gen_and_plot.m
+++ Matlab/gen_and_plot.m
@@ -3,16 +3,11 @@ fs = 4;
 dt=1/fs;
 stoptime = 60;
 sample_length = (fs*stoptime)+1;
-
+t = (0:dt:stoptime)'; % seconds 
 
 S = oceanWaveSim(num_waves, 1, fs, stoptime);
 %plotFFT(fs, sample_length, S);
 
-
-%Plot surface displacement
-%figure(3)
-t = (0:dt:stoptime)'; % seconds 
-plot(t,S);
 
 sig_wave_height = sigWaveCalc(S);
 fprintf('Simulated wave sig wave height (4*std_dev) = %f\n',4*std(sim_wave));
@@ -20,8 +15,4 @@ fprintf('Simulated wave sig wave height (highest 3rd) = %f\n',sig_wave_height);
 
 yline(sig_wave_height/2,'--g','Hightest 3rd Calc');
 yline((4*std(S))/2,'b','4*std Calc');
-
-
-%yline((4*std(S))- abs(min(S)));
-% Find local maxima and minima
 
