8d31e12349a5d0fac525c8b03391e06e0f52169f
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-20 15:12:04 +0000

    fixed typo

--- Matlab/plotFFT.m
+++ Matlab/plotFFT.m
@@ -1,4 +1,4 @@
-function P p = plotFFT(Fs, sample_length, S)
+function p = plotFFT(Fs, sample_length, S);
                 
     T = 1/Fs;             % Sampling period       
     L = sample_length;            % Length of signal
@@ -11,9 +11,11 @@ function P p = plotFFT(Fs, sample_length, S)
     P1 = P2(1:L/2+1);
     P1(2:end-1) = 2*P1(2:end-1);
 
-
+    figure(2)
     f = Fs*(0:(L/2))/L;
     plot(f,P1) 
+    grid
+    A = trapz(f, P1)
     title('Single-Sided Amplitude Spectrum of X(t)')
     xlabel('f (Hz)')
     ylabel('|P1(f)|')
