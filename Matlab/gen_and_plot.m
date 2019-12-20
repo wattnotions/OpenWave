0fae0ade422e46b53c752edf3c41c389d92474b6
Shane Ormonde <Shane.Ormonde@itb.ie>
2019-12-20 15:14:27 +0000

    added local extema detection

--- Matlab/gen_and_plot.m
+++ Matlab/gen_and_plot.m
@@ -13,5 +13,24 @@ plotFFT(fs, sample_length, S);
 figure(3)
 t = (0:dt:stoptime)'; % seconds 
 
-plot(t,S);
-yline((4*std(S))- abs(min(S)));
+%plot(t,S);
+%yline((4*std(S))- abs(min(S)));
+% Find local maxima and minima
+maxIndices = islocalmax(S);
+minIndices = islocalmin(S);
+
+% Visualize results
+clf
+plot(S,'Color',[109 185 226]/255,'DisplayName','Input data')
+hold on
+
+% Plot local maxima
+plot(find(maxIndices),S(maxIndices),'^','Color',[217 83 25]/255,...
+    'MarkerFaceColor',[217 83 25]/255,'DisplayName','Local maxima')
+
+% Plot local minima
+plot(find(minIndices),S(minIndices),'v','Color',[237 177 32]/255,...
+    'MarkerFaceColor',[237 177 32]/255,'DisplayName','Local minima')
+title(['Number of extrema: ' num2str(nnz(maxIndices)+nnz(minIndices))])
+hold off
+legend
