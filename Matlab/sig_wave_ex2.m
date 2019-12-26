aca18a314ed93675292abbe1b153cdf088664893
shane <shane@localhost.localdomain>
2019-12-26 23:55:51 +0000

    added variance/area comparison

--- Matlab/sig_wave_ex2.m
+++ Matlab/sig_wave_ex2.m
@@ -1,12 +1,20 @@
 
-
-
-num_waves = 300;
-fs = 60;
+num_waves = 100;
+fs = 10;
 dt=1/fs;
-stoptime = 60;
+stoptime = 40;
 sample_length = (fs*stoptime)+1;
+
 t = (0:dt:stoptime)'; % seconds 
 
 S = oceanWaveSim(num_waves, 1, fs, stoptime);
-plotFFT(fs, sample_length, S);
+psd_area = psd_test(S, fs);
+
+
+fprintf('Elevation data signal variance = %0.2f\n',var(S));
+fprintf('PSD area under curve = %0.2f\n',psd_area);
+
+
+
+figure(4)
+plot(t,S);
