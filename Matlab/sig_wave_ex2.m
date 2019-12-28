9dc369ce857ae49e177d20003ca95a54c02064b5
shane <shane@localhost.localdomain>
2019-12-28 18:38:14 +0000

    added sig wave height calc

--- Matlab/sig_wave_ex2.m
+++ Matlab/sig_wave_ex2.m
@@ -1,3 +1,9 @@
+%{
+Generates simulated ocean elavation data
+Calculates significant wave height in the time domain and the freq domain
+(Area under PSD curve is equal to variance of time domain signal)
+%}
+
 
 num_waves = 100;
 fs = 10;
@@ -13,6 +19,9 @@ psd_area = psd_test(S, fs);
 
 fprintf('Elevation data signal variance = %0.2f\n',var(S));
 fprintf('PSD area under curve = %0.2f\n',psd_area);
+
+fprintf('Significant wave height (time domain calc) = %0.2f\n',4*sqrt(var(S)));
+fprintf('Significant wave height (freq domain calc) = %0.2f\n',4*sqrt(psd_area));
 
 
 
