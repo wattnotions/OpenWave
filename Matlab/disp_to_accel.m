d1bf39ab9bc89d662ed025ca4401d74559d2701f
shane <shane@localhost.localdomain>
2019-12-28 21:21:56 +0000

    added high pass filter

--- Matlab/disp_to_accel.m
+++ Matlab/disp_to_accel.m
@@ -1,7 +1,7 @@
 num_waves = 30;
 fs = 40;
 dt=1/fs;
-stoptime = 200;
+stoptime = 100;
 sample_length = (fs*stoptime)+1;
 
 t = (0:dt:stoptime)'; % seconds 
@@ -11,6 +11,11 @@ S = oceanWaveSim(num_waves, 1, fs, stoptime);
 accel = disp2Accel(S, fs);
 
 disp = accel2Disp(accel, fs);
+disp = highpass(disp,0.03,fs);
+
+disp = disp - mean(disp);
+S = S-mean(S);
+%disp = acc2disp(accel, dt);
 
 figure(3)
 hold off
