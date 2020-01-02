576ab34d6015ea6086a18cec57ac286973ebb2e3
Shane Ormonde <Shane.Ormonde@itb.ie>
2020-01-02 14:53:53 +0000

    changed float to print only 2 decimal places

--- Matlab/sig_wave_ex1.m
+++ Matlab/sig_wave_ex1.m
@@ -6,24 +6,27 @@ It then plots the wave data with these two measurements overlayed
 %}
 
 
-num_waves = 10000;
-fs = 4;
+num_waves = 300;
+fs = 3;
 dt=1/fs;
 stoptime = 60;
 sample_length = (fs*stoptime)+1;
 t = (0:dt:stoptime)'; % seconds 
 
-S = oceanWaveSim(num_waves, 1, fs, stoptime);
+S = oceanWaveSim(num_waves, 0.1, fs, stoptime);
 
 sig_wave_height = sigWaveCalc(S);
 sig_wave_std_dev = 4*std(S);
-fprintf('Simulated wave sig wave height (4*std_dev) = %f\n',4*std(S));
-fprintf('Simulated wave sig wave height (highest 3rd) = %f\n',sig_wave_height);
+fprintf('Simulated wave sig wave height (4*std_dev) = %0.2f\n',4*std(S));
+fprintf('Simulated wave sig wave height (highest 3rd) = %0.2f\n',sig_wave_height);
 
 percent_diff = ((sig_wave_std_dev - sig_wave_height) / sig_wave_height)*100;
-fprintf('Percent difference = %f\n', percent_diff);
+fprintf('Percent difference = %0.2f\n', percent_diff);
 
 
+figure(1)
 plot(t,S);
 yline(sig_wave_height/2,'--g','Hightest 3rd Calc');
 yline((4*std(S))/2,'b','4*std Calc');
+
+
