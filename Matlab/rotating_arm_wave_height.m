1c8d917c347f5322edf873d1b2592de76ef8d4f6
Shane Ormonde <Shane.Ormonde@itb.ie>
2020-01-02 12:02:21 +0000

    changed script to use time from csv file

--- Matlab/rotating_arm_wave_height.m
+++ Matlab/rotating_arm_wave_height.m
@@ -1,10 +1,11 @@
 
 fs = 20;
 
-%%% real accel data test
+%%% read accel data from csv file
 T = readtable('../test_data/rotating_arm_data/20cm_3v.csv');
 z_accel = table2array(T(:,3)); %z_accel
-t = table2array(T(:,9)); %timestamps
+timestamps = table2array(T(:,9)); %timestamps
+t = formatTimestamp(timestamps); %X axis (milliseconds)
 z_accel = lowpass(z_accel,0.15, fs,'Steepness',0.96); %filter out high freq noise
 
 %plot filtered z_accel
@@ -14,14 +15,14 @@ title('Z axis acceleration');
 xlabel('Time (Seconds)');
 
 z_accel = z_accel - mean(z_accel); %remove dc offset
-disp = accel2Disp(z_accel, fs);    %convert accel to displacement
+disp = accel2Disp(z_accel, fs,t);    %convert accel to displacement
 disp = disp-mean(disp);             
 
 %remove low freq noise caused by double integration
 disp = highpass(disp,0.15, fs,'Steepness',0.96);
 plotFFT(fs, length(disp), disp)
 
-fprintf('Sig wave height = %0.2f\n',sigWaveCalc(disp));
+fprintf('Sig wave height (highest 3rd) = %0.2f\n',sigWaveCalc(disp));
 
 %plot displacement signal
 figure(1003)
