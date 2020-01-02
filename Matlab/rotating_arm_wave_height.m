355a994826c7d71fd5374613a4a481144a5e6d71
Shane Ormonde <Shane.Ormonde@itb.ie>
2020-01-02 16:40:26 +0000

    changed highpass cutoff to 0,1Hz

--- Matlab/rotating_arm_wave_height.m
+++ Matlab/rotating_arm_wave_height.m
@@ -6,7 +6,7 @@ T = readtable('../test_data/rotating_arm_data/20cm_3v.csv');
 z_accel = table2array(T(:,3)); %z_accel
 timestamps = table2array(T(:,9)); %timestamps
 t = formatTimestamp(timestamps); %X axis (milliseconds)
-z_accel = lowpass(z_accel,0.15, fs,'Steepness',0.96); %filter out high freq noise
+%z_accel = lowpass(z_accel,0.2, fs,'Steepness',0.96); %filter out high freq noise
 
 %plot filtered z_accel
 figure(2000)
@@ -18,8 +18,9 @@ z_accel = z_accel - mean(z_accel); %remove dc offset
 disp = accel2Disp(z_accel, fs,t);    %convert accel to displacement
 disp = disp-mean(disp);             
 
+
 %remove low freq noise caused by double integration
-disp = highpass(disp,0.15, fs,'Steepness',0.96);
+disp = highpass(disp,0.1, fs,'Steepness',0.96);
 plotFFT(fs, length(disp), disp)
 
 fprintf('Sig wave height (highest 3rd) = %0.2f\n',sigWaveCalc(disp));
