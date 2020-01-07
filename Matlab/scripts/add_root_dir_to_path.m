c9c4e324f76983fffe6e664526a72b91ac8ae2e7
Shane Ormonde <Shane.Ormonde@itb.ie>
2020-01-07 09:15:22 +0000

    changed folder structure

---
+++ Matlab/scripts/add_root_dir_to_path.m
@@ -0,0 +1,8 @@
+%{
+This script adds the 'Matlab' folder to matlabs search path
+Change root dir variable to reflect the location on your PC
+root dir should point to the 'matlab' folder
+%}
+
+root_dir = 'C:\Users\Shane Ormonde\Documents\OpenWave\Matlab';
+addpath(genpath(root_dir)); %add matlab folder and all sub folders
