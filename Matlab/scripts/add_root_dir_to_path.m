0fca689ad5f7e17e3c608934471df0f3cd271fe5
wattnotions <shaneormonde@gmail.com>
2020-01-07 12:53:15 +0000

    Update add_root_dir_to_path.m

--- Matlab/scripts/add_root_dir_to_path.m
+++ Matlab/scripts/add_root_dir_to_path.m
@@ -4,5 +4,5 @@ Change root dir variable to reflect the location on your PC
 root dir should point to the 'matlab' folder
 %}
 
-root_dir = 'C:\Users\Shane Ormonde\Documents\OpenWave\Matlab';
+root_dir = 'C:\Users\User1\Documents\OpenWave\Matlab';
 addpath(genpath(root_dir)); %add matlab folder and all sub folders
