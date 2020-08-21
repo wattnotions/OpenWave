7b6792db8187b513e3acb5ce1b8c1664917317e1
wattnotions <shaneormonde@gmail.com>
2020-08-21 17:52:34 +0100

    added calib data read to file

--- pyboard_BN055/main.py
+++ pyboard_BN055/main.py
@@ -3,7 +3,11 @@ from machine import Pin, I2C, UART
 import time
 
 i2c = I2C(1,freq=100000)
-
+calib_data = i2c.readfrom_mem(0x28, 0x55, 22)
+print(calib_data)
+f = open('calibration.txt', 'w')
+f.write(calib_data)
+f.close()
 
 s = bno055.BNO055(i2c)
 print(s.temperature())
@@ -12,14 +16,16 @@ print(s.temperature())
 
 uart = UART(3, 115200)                         # init with given baudrate
 
-while True:
-    uart.write('hello\r\n')
-    print("Sent hello")
-    time.sleep(1)
 
 
-while True:
-    print(s.euler(), end='\r')
-    time.sleep_ms(100)
-    print('\x1b[2K\r', end='')
-    
+
+#while True:
+#    uart.write('hello\r\n')
+#    print("Sent hello")
+#    time.sleep(1)
+
+
+#while True:
+#    print(s.euler(), end='\r')
+#    time.sleep_ms(100)
+#    print('\x1b[2K\r', end='')
