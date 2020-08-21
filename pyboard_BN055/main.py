d8c0a79284806c96ffa97f0dd304b5edfd0b773b
wattnotions <shaneormonde@gmail.com>
2020-08-21 23:31:47 +0100

    started work on adding timer

--- pyboard_BN055/main.py
+++ pyboard_BN055/main.py
@@ -3,12 +3,17 @@ from machine import Pin, I2C, UART
 import time
 from binascii import hexlify
 
-
-
-
-
 i2c = I2C(1,freq=100000)
 s = bno055.BNO055(i2c)
+
+def print_linaccel(tim4):
+    print(s.linear_acceleration())
+
+tim4 = pyb.Timer(4, freq=10)
+tim4.callback(print_linaccel)
+
+
+
 
 s.load_calibration_data()
 print(s.temperature())
