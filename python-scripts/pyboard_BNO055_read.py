7efe710a77f61dd15556442be2433517f03cc95f
wattnotions <shaneormonde@gmail.com>
2020-09-02 10:15:23 +0100

    added save to csv file

--- python-scripts/pyboard_BNO055_read.py
+++ python-scripts/pyboard_BNO055_read.py
@@ -3,5 +3,8 @@ import serial
 ser = serial.Serial('COM18', 115200, timeout = 1)
 
 
-while True:
-    print (ser.readline())
+with open('../test_data/pyboard_bno055/bno055_data.csv','w') as f:
+
+    while True:
+        f.write(ser.readline().decode())
+        
