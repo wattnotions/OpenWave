943e3e463e411b6acbfdd312675455cda96768d3
wattnotions <shaneormonde@gmail.com>
2021-02-09 11:55:51 +0000

    added step to mm test code

--- arduino/linear_rail_driver/linear_rail_driver.ino
+++ arduino/linear_rail_driver/linear_rail_driver.ino
@@ -12,6 +12,11 @@
 // give it a name:
 int led = 13;
 
+#define END_STOP digitalRead(12) // 1 is not pressed 0 is pressed
+#define OFF 1
+#define ON 0
+
+
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
@@ -23,64 +28,108 @@ void setup() {
 
   //end stop
   pinMode(12,INPUT_PULLUP);
+
+  Serial.begin(9600);
   
 }
 
 // the loop routine runs over and over again forever:
+
+int step_count,i;
 void loop() {
 
-  Serial.print(digitalRead(12));
-  delay(500);
-   
-  
-  
-  
+  go_home();
+
+  while(1){
+    Serial.println("START OF WHILE LOOP");
+    i=0;
+    while(step_count < 7000){
+      
+      up(5000);
+      Serial.println(step_count);
+      if(END_STOP == ON) {
+        Serial.println("END STOP PRESSED");
+        while(1){};
+        }
+    }
+
+    i=0;
+    Serial.println("OUT OF FIRST LOOP");
+    while(step_count > 0){
      
+      down(5000);
+      Serial.println(step_count);
+      if(END_STOP == ON) {
+        Serial.println("END STOP PRESSED");
+        while(1){};
+        }
+    }
+    
+  }
   
+  while(1){}
 
   
-
-
-
-  
-             
 }
 
+void go_home(void){
+  
+  while(END_STOP == 1){
+    down(5000);
+    Serial.println(step_count);
+  }
 
-void forward(int delay_us){
+
+  while(END_STOP == 0){
+    up(5000);
+    Serial.println(step_count);
+  }
+
+  step_count = 0;
+}
+
+void down(int delay_us){
   digitalWrite(2, HIGH);   
   digitalWrite(3, LOW);  
   delayMicroseconds(delay_us); 
-
+  step_count--;
+  
   digitalWrite(4, HIGH);   
   digitalWrite(5, LOW);
   delayMicroseconds(delay_us);
+  step_count--;
 
   digitalWrite(2, LOW);   
   digitalWrite(3, HIGH);
   delayMicroseconds(delay_us);
+  step_count--;
     
   digitalWrite(4, LOW);   
   digitalWrite(5,HIGH);
   delayMicroseconds(delay_us);   
+  step_count--;
 }
 
-void reverse(int delay_us){
+void up(int delay_us){
   digitalWrite(4, HIGH);   
   digitalWrite(5, LOW);
   delayMicroseconds(delay_us);
+  step_count++;
   
   digitalWrite(2, HIGH);   
   digitalWrite(3, LOW);  
   delayMicroseconds(delay_us); 
-
+  step_count++;
+  
   digitalWrite(4, LOW);   
   digitalWrite(5,HIGH);
   delayMicroseconds(delay_us);  
+  step_count++;
 
   digitalWrite(2, LOW);   
   digitalWrite(3, HIGH);
   delayMicroseconds(delay_us);
+  step_count++;
     
     
 }
